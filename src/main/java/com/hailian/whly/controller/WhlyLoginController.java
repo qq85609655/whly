12c795c222b80584e1596a614dd623add6090cdf
zuoqingbei <138898564@qq.com>
2017-09-29 17:38:57 +0800

    提交

--- src/main/java/com/hailian/whly/controller/WhlyLoginController.java
+++ src/main/java/com/hailian/whly/controller/WhlyLoginController.java
@@ -1,15 +1,13 @@
 package com.hailian.whly.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.hailian.whly.entity.WhlyAccount;
 import com.hailian.whly.service.WhlyAccountService;
 import com.thinkgem.jeesite.common.web.BaseController;
 
@@ -27,12 +25,13 @@ public class WhlyLoginController extends BaseController {
 	private WhlyAccountService service;
 
 	@RequestMapping("/login")
-	public String login(HttpServletRequest request, HttpServletResponse response) {
-		List<WhlyAccount> list=service.getWhlyAccountList(null);
+	public String login(HttpServletRequest request, HttpServletResponse response,Model model) {
+	/*	List<WhlyAccount> list=service.getWhlyAccountList(null);
 		for(WhlyAccount a :list){
 			System.out.println(a.getId());
-		}
-		return whlyPath+"/user/index";
+		}*/
+		model.addAttribute("id", "home");
+		return whlyPage+"/user/login";
 	}
 
 }
