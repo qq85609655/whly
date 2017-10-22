0e9fd55b678d68dbf0d8ecaf08a37a2782a57c62
zuoqingbei <138898564@qq.com>
2017-10-22 20:00:52 +0800

    添加审批枚举

--- src/main/java/com/hailian/whly/controller/WhlyTestController.java
+++ src/main/java/com/hailian/whly/controller/WhlyTestController.java
@@ -32,5 +32,51 @@ public class WhlyTestController extends BaseController {
 	public String news(HttpServletRequest request, HttpServletResponse response,Model model) {
 		return whlyPage+"/test/news";
 	}
-
+	/* public Map<String, Object> getTopReportMonth()
+			      throws Exception
+			    {
+			   90      LoginUser user = User.getCurrentUser();
+			   91      Map result = new HashMap();
+			   92      if (user == null) {
+			   93        throw new BusinessException("当前登陆用户为空，请重新登录");
+			      }
+			   95      BdmCompany company = user.getCompany();
+			   96      if (company == null) {
+			   97        throw new BusinessException("当前登陆用户无所属企业，请重新登录");
+			      }
+			   99      Calendar now = Calendar.getInstance();
+			  100      Integer year = Integer.valueOf(now.get(1));
+			  101      Integer month = Integer.valueOf(now.get(2) + 1);
+			  
+			  103      CompanyMonthReport report = CompanyMonthReport.getTopMonthReport(company.getId());
+			  104      if (report == null) {
+			  105        result.put("year", year);
+			  106        result.put("month", Integer.valueOf(month.intValue() - 1));
+			  107        result.put("info", year + "年" + (month.intValue() - 1) + "月");
+			  108        return result;
+			      }
+			  110      if (report.getMonth().equals(Integer.valueOf(12))) {
+			  111        if ((report.getYear().equals(year)) && (report.getMonth().equals(Integer.valueOf(month.intValue() - 1)))) {
+			  112          result.put("year", Integer.valueOf(report.getYear().intValue() + 1));
+			  113          result.put("month", "01");
+			  114          result.put("info", report.getYear().intValue() + 1 + "年01月");
+			  115          return result;
+			        }
+			  117        result.put("year", Integer.valueOf(report.getYear().intValue() + 1));
+			  118        result.put("month", "01");
+			  119        result.put("info", report.getYear().intValue() + 1 + "年01月");
+			  120        return result;
+			      }
+			  
+			  123      if ((report.getYear().equals(year)) && (report.getMonth().intValue() >= month.intValue() - 2)) {
+			  124        result.put("year", report.getYear());
+			  125        result.put("month", Integer.valueOf(report.getMonth().intValue() + 1));
+			  126        result.put("info", report.getYear() + "年" + (report.getMonth().intValue() + 1) + "月");
+			  127        return result;
+			      }
+			  129      result.put("year", report.getYear());
+			  130      result.put("month", Integer.valueOf(report.getMonth().intValue() + 1));
+			  131      result.put("info", report.getYear() + "年" + (report.getMonth().intValue() + 1) + "月");
+			  132      return result;
+			    }*/
 }
